5e3623237d33395d9f7e92ca0c0c532575d9ebb2
IBLIVE <92832071+IBLIVE@users.noreply.github.com>
2021-11-05 04:38:59 +0300

    Update Source3.cpp

--- HW7/Source3.cpp
+++ HW7/Source3.cpp
@@ -35,7 +35,7 @@ int main() {
 	for (int i(0); i < 3; i++) {
 		for (int j(0); j < 3; j++) {
 			for (int k(0); k < 3; k++) {
-				arr[i][j] = arr1[i][k] * arr2[k][j];
+				arr[i][j] += arr1[i][k] * arr2[k][j];
 			}
 		}
 	}
