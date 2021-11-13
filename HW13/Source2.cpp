49982dc3984017c5bdd4cd0932f3fbae59a109f0
Asanali Bulatov <kazgameplay@gmail.com>
2021-11-13 15:59:46 +0600

    HW13_2 another variant

--- HW13/Source2.cpp
+++ HW13/Source2.cpp
@@ -1,4 +1,4 @@
-#include <conio.h>
+//#include <conio.h>
 #include <iostream>
 
 using namespace std;
@@ -12,7 +12,6 @@ void concatenation(char *str1, char *str2) {
 			i++;
 			continue;
 		}
-		cout << "\n";
 		if (str2[j] != '\0') {
 			str1[i] = str2[j];
 			i++;
@@ -25,33 +24,42 @@ void concatenation(char *str1, char *str2) {
 	}
 }
 int main() {
-	
-	int i = 0;
-	char str1[100] = "Azamat ";
-	char str2[30] = "16 let";
+	char str1[2000];
+	char str2[2000];
 
-	/*cout << "Enter first line: ";
+	// char str1[100] = "Azamat ";
+	// char str2[30] = "16 let";
+
+	cout << "Enter first line: ";
 	cin >> str1;
-	system("cls");
-	cin.clear();
 
 	cout << "Enter second line: ";
 	cin >> str2;
-	system("cls");
-	cin.clear();*/ 
+
+	concatenation(str1, str2);
+
+
+	cout << "str1: " << str1 << endl;
+	cout << "str2: " << str2 << endl;
+
 	//Асан, когда будешь чекать, то убери тут скобки и введи значение массивов с консоли.
 	//У меня при подобном выводе багалась строка и выводились ненужные элементы, в чем проблема?
 
-	/*while (str1[i] != '\0') {
+		
+	// int i = 0;
+	// while (str1[i] != '\0') {
+	// 	cout << str1[i];
+	// 	i++;
+	// }
+	//если захочешь протестить массив после ввода
 
-		cout << str1[i];
-		i++;
-	}*/ //если захочешь протестить массив после ввода
-	cout  << str1 <<"\n";
-	cout << str2 << "\n";
-	concatenation(str1, str2);
-	cout << str1;
 
-	_getch();
+	// cout  << str1 <<"\n";
+	// cout << str2 << "\n";
+
+
+
+	// cout << str1;
+
 	return 0;
 }
